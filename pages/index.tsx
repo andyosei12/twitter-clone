e163a3c315030987410877556c984588dfd12b4a
Andy Osei <andrew.owusu@novanest.net>
2022-05-10 18:32:40 +0000

    add refresh feed functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
+import { Toaster } from 'react-hot-toast'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
@@ -25,6 +26,8 @@ const Home = ({ tweets }: Props) => {
       <Head>
         <title>Twitter clone</title>
       </Head>
+
+      <Toaster />
 
       <main className="grid grid-cols-9">
         {/* Sidebar */}
